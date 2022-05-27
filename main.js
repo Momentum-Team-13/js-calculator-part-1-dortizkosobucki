51a8352e385dca32dd554cebf351ea6f885d1b00
daniela <d.ortizkosobucki@gmail.com>
2022-05-27 18:22:44 -0400

    eval finish

--- main.js
+++ main.js
@@ -1,67 +1,91 @@
 // const { speedOfLightDependencies } = require("mathjs");
 
+// const math = require("mathjs");
+
 //definitions
-let numberArr = document.querySelectorAll('.number');
-let operationArr = document.querySelectorAll('.operation');
+let numberArr = document.querySelectorAll('.button');
 let display = document.querySelector('.display');
 let clearAll = document.querySelector('.clear');
-let total = document.querySelector('.equal')
+let equals = document.getElementById("e");
 
-//numbers+operation storage
-let number1 = '';
-let number2 = '';
-let nextOperation = ''; //
 
-//event listen numbers
-for (let num of numberArr) {
-    num.addEventListener("click", function (event) {
-        if (number1 && nextOperation) {
-            number2 += event.target.innerText;
-            display.innerText += event.target.innerText;
+for (let number of numberArr) {
+    number.addEventListener("click", function (event) {
+        console.log(number.innerText)
+        if (number === equals) {
+            display.innerText = math.eval(display.innerText);
         } else {
-            number1 += event.target.innerText;
-            display.innerText += event.target.innerText;
+            display.innerText += event.target.textContent;
         }
-        console.log(event.target.innerText)
-    });
-}
-
-//event listen operation 
-for (let operation of operationArr) {
-    operation.addEventListener('click', function (event) {
-        if (number1) {
-            nextOperation = event.target.innerText;
-            display.innerText += " " + event.target.innerText;
-        }
-        console.log(event.target.innerText);
     })
 }
-
-//store total as number1
-
 
 //event listen clear
 clearAll.addEventListener("click", function (event) {
     display.innerText = " ";
 })
 
-//event listen total
-total.addEventListener("click", function (event) {
-    if (number1 && number2) {
-        calculation();
-    }
-});
+
+//numbers+operation storage
+// let number1 = '';
+// let number2 = '';
+// let nextOperation = ''; //
 
 
-// // //function calculation 
-function calculation() {
-    if (nextOperation === "+") {
-        solution = (parseFloat(number1) + parseFloat(number2))
-        display.innerText = solution
-        console.log(solution)
-    } else if (nextOperation === "-") {
-        solution = (parseFloat(number1) - parseFloat(number2))
-        display.innerText = solution
-        console.log(solution)
-    }
-}
+
+// //event listen numbers
+// for (let num of numberArr) {
+//     num.addEventListener("click", function (event) {
+//         if (number1 && nextOperation) {
+//             number2 += event.target.innerText;
+//             display.innerText += event.target.innerText;
+//         } else {
+//             number1 += event.target.innerText;
+//             display.innerText += event.target.innerText;
+//         }
+//         console.log(event.target.innerText)
+//     });
+// }
+
+// //event listen operation 
+// for (let operation of operationArr) {
+//     operation.addEventListener('click', function (event) {
+//         if (number1) {
+//             nextOperation = event.target.innerText;
+//             display.innerText += " " + event.target.innerText;
+//         }
+//         console.log(event.target.innerText);
+//     })
+// }
+
+//store total as number1
+
+
+// //event listen total
+// total.addEventListener("click", function (event) {
+//     if (number1 && number2) {
+//         calculation();
+//     }
+// });
+
+
+// // // //function calculation 
+// function calculation() {
+//     if (nextOperation === "+") {
+//         solution = (parseFloat(number1) + parseFloat(number2))
+//         display.innerText = solution
+//         console.log(solution)
+//     } else if (nextOperation === "-") {
+//         solution = (parseFloat(number1) - parseFloat(number2))
+//         display.innerText = solution
+//         console.log(solution)
+//     } else if (nextOperation === "X") {
+//         solution = (parseFloat(number1) * parseFloat(number2))
+//         display.innerText = solution
+//         console.log(solution)
+//     } else if (nextOperation === "/") {
+//         solution = (parseFloat(number1) / parseFloat(number2))
+//         display.innerText = solution
+//         console.log(solution)
+//     }
+// }
